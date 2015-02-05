29ed385b81292344b70bb02a27231e5039b71cbc
释然 <shiran@taobao.com>
2015-02-05 14:05:09 +0800

    change comment and add copyright to minfile

--- build/event-base-debug.js
+++ build/event-base-debug.js
@@ -1,8 +1,4 @@
-/*
-Copyright 2014, modulex-event-base@1.0.2
-MIT Licensed
-build time: Thu, 16 Oct 2014 04:19:33 GMT
-*/
+//! Copyright 2015, kissy-event-base@1.0.3 MIT Licensed, build time: Thu, 05 Feb 2015 06:04:53 GMT 
 modulex.add("event-base", ["modulex-util"], function(require, exports, module) {
 var modulexUtil = require("modulex-util");
 /*
@@ -235,7 +231,7 @@ eventBase = function (exports) {
   var Observer = eventBaseObserver;
   var Observable = eventBaseObservable;
   exports = {
-    version: '1.0.2',
+    version: '1.0.3',
     Utils: Utils,
     Object: eventBaseObject,
     Observer: Observer,
